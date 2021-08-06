2f87a01fef32ab36a9823dc21a68b1cb9baa9061
Dhananjay Mayane <dhananjay.mayane@hbwsl.com>
2021-08-06 15:04:02 +0530

    Auc 24 (#55) (#56)
    * Added tabs for custom product types
    
    * Added js for relist and extend options for auction.
    
    * Updated code to save products.
    
    * Displayed product in frontend.
    
    * Commented: Cannot redeclare tab error thrown by test case
    
    * Added a condition for redeclaration of auction history tab
    
    * Added a condition for redeclaration of auction history tab
    
    * Added hooks if WCFM class exists.
    
    * Removed auction history tab from WCFM
    
    * Added a condition for declaring auction history
    
    * Added alerts according to user input in the form.
    
    Co-authored-by: Sonali Londhe <81282336+sonali-londhe@users.noreply.github.com>

--- public/partials/single-product/auction-software-simple.php
+++ public/partials/single-product/auction-software-simple.php
@@ -64,7 +64,7 @@ do_action( 'auction_simple_before_add_to_cart_form' );
 	<input id="auction_current_bid" type="hidden" value="<?php echo esc_attr( $product->get_auction_current_bid() ); ?>"/>
 	<input id="auction_initial_bid_placed" type="hidden" value="<?php echo esc_attr( WC_Auction_Software_Helper::get_auction_post_meta( $postid, 'auction_initial_bid_placed' ) ); ?>"/>
 	<input id="auction_bid_increment" type="hidden" value="<?php echo esc_attr( $product->get_auction_bid_increment() ); ?>"/>
-	<?php if ( '' === $product->get_auction_errors() ) { ?>
+	<?php if ( '' === $product->get_auction_errors() || 0 === $product->get_auction_errors() ) { ?>
 		<?php if ( true === $product->is_started() && 1 !== (int) $product->get_auction_is_sold() && false === $product->is_ended() ) { ?>
 			<table cellspacing="0">
 				<tbody>
@@ -266,18 +266,19 @@ do_action( 'auction_simple_before_add_to_cart_form' );
 			 * @return mixed
 			 */
 			function auction_history_tab( $tabs ) {
-				$tabs['auction_history_tab'] = array(
-					'title'    => __( 'Auction History', 'auction-software' ),
-					'priority' => 1,
-					'callback' => 'auction_history_tab_content',
-				);
+					$tabs['auction_history_tab'] = array(
+						'title'    => __( 'Auction History', 'auction-software' ),
+						'priority' => 1,
+						'callback' => 'auction_history_tab_content',
+					);
 
-				return $tabs;
+					return $tabs;
 			}
 
 			/**
 			 * History tab content.
 			 */
+
 			function auction_history_tab_content() {
 				echo '<h2>' . esc_html__( 'Auction History', 'auction-software' ) . '</h2>';
 				echo WC_Auction_Software_Helper::get_auction_history( get_the_ID() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
---
+++ wcfm/class-wcfm-auction-software-helper.php
@@ -0,0 +1,521 @@
+<?php
+/**
+ * Auction Software Helper class.
+ *
+ * @since 1.0.0
+ *
+ * @package    Auction_Software
+ * @subpackage Auction_Software/wcfm
+ */
+
+/**
+ * Auction Software Helper class.
+ *
+ * @package    Auction_Software
+ * @subpackage Auction_Software/wcfm
+ */
+class WCFM_Auction_Software_Helper {
+
+	/**
+	 * Output product tab fields.
+	 *
+	 * @param string $input_type Input type.
+	 * @param string $id Field id.
+	 * @param string $label Field label.
+	 * @param bool   $desc_tip Help tip.
+	 * @param string $description Help tip description.
+	 * @param bool   $currency Currency.
+	 * @param string $options Options.
+	 * @param array  $custom_attributes Custom attributes.
+	 * @param string $class Class.
+	 * @param string $wrapper_class Wrapper class.
+	 * @return int|void
+	 */
+	public static function get_product_tab_fields( $input_type, $id, $label, $desc_tip, $description, $currency = false, $options = '', $custom_attributes = array(), $class = '', $wrapper_class = '' ) {
+		switch ( $input_type ) {
+			case 'text':
+				if ( ( 'date_from' === $id || 'date_to' === $id ) ) {
+					return array(
+						'auction_' . $id => array(
+							'label'             => self::get_id_title( $id, $label, $currency ),
+							'label_class'       => 'wcfm_title',
+							'type'              => 'text',
+							'custom_attributes' => $custom_attributes,
+							'class'             => 'wcfm-text wcfm_ele auction_simple auction_reverse auction_penny' . $class,
+							'wrapper_class'     => $wrapper_class,
+							'desc_tip'          => $desc_tip,
+							'description'       => $description,
+						),
+					);
+				} elseif ( ( 'reserve_price' === $id || 'buy_it_now_price' === $id ) ) {
+					return array(
+						'auction_' . $id =>
+						array(
+							'label'             => self::get_id_title( $id, $label, $currency ),
+							'label_class'       => 'wcfm_title wcfm_ele auction_simple',
+							'custom_attributes' => $custom_attributes,
+							'class'             => 'wcfm-text wcfm_ele auction_simple' . $class,
+							'wrapper_class'     => $wrapper_class,
+							'type'              => 'text',
+							'desc_tip'          => $desc_tip,
+							'description'       => $description,
+						),
+					);
+				} elseif ( ( 'reserve_price_reverse' === $id || 'buy_it_now_price_reverse' === $id ) ) {
+					return array(
+						'auction_' . $id =>
+						array(
+							'label'             => self::get_id_title( $id, $label, $currency ),
+							'label_class'       => 'wcfm_title wcfm_ele auction_reverse',
+							'custom_attributes' => $custom_attributes,
+							'class'             => 'wcfm-text wcfm_ele auction_reverse' . $class,
+							'wrapper_class'     => $wrapper_class,
+							'type'              => 'text',
+							'desc_tip'          => $desc_tip,
+							'description'       => $description,
+						),
+					);
+				} else {
+					return array(
+						'auction_' . $id =>
+						array(
+							'label'             => self::get_id_title( $id, $label, $currency ),
+							'label_class'       => $wrapper_class . 'wcfm_title',
+							'custom_attributes' => $custom_attributes,
+							'class'             => $wrapper_class . 'wcfm-text wcfm_ele auction_simple auction_reverse auction_penny' . $class,
+							'wrapper_class'     => $wrapper_class,
+							'type'              => 'text',
+							'desc_tip'          => $desc_tip,
+							'description'       => $description,
+						),
+					);
+				}
+			case 'select':
+				$options        = explode( ',', $options );
+				$select_options = array();
+				foreach ( $options as $option_value ) {
+					$select_options[ $option_value ] = self::get_id_title( $option_value );
+				}
+				return array(
+					'auction_' . $id => array(
+						'type'              => 'select',
+						'label_class'       => $wrapper_class . 'wcfm_title',
+						'options'           => $select_options,
+						'label'             => self::get_id_title( $id, $label ),
+						'custom_attributes' => $custom_attributes,
+						'class'             => $wrapper_class . 'wcfm-select wcfm_ele auction_simple auction_reverse auction_penny' . $class,
+						'wrapper_class'     => $wrapper_class,
+						'desc_tip'          => $desc_tip,
+						'description'       => $description,
+					),
+				);
+			case 'date':
+				return array(
+					'auction_' . $id =>
+					array(
+						'label'             => self::get_id_title( $id, $label, $currency ),
+						'label_class'       => $wrapper_class . 'wcfm_title',
+						'custom_attributes' => $custom_attributes,
+						'type'              => 'date',
+						'date-type'         => 'years',
+						'class'             => $wrapper_class . 'wcfm-text wcfm_ele auction_simple auction_reverse auction_penny' . $class,
+						'wrapper_class'     => $wrapper_class,
+						'desc_tip'          => $desc_tip,
+						'description'       => $description,
+					),
+				);
+			case 'checkbox':
+				return array(
+					'auction_' . $id =>
+					array(
+						'label'         => self::get_id_title( $id, $label, $currency ),
+						'label_class'   => $wrapper_class . 'wcfm_title wcfm-checkbox',
+						'class'         => $wrapper_class . 'wcfm-checkbox wcfm_ele auction_simple auction_reverse auction_penny' . $class,
+						'wrapper_class' => $wrapper_class,
+						'desc_tip'      => $desc_tip,
+						'description'   => $description,
+						'type'          => 'checkbox',
+						'value'         => 'yes',
+					),
+				);
+			default:
+				return 0;
+		}
+	}
+
+	/**
+	 * Get product field label with currency symbol.
+	 *
+	 * @param int    $id ID.
+	 * @param string $label Field label.
+	 * @param bool   $currency Currency.
+	 * @return string
+	 */
+	public static function get_id_title( $id, $label = '', $currency = false ) {
+		$id_string = ucwords( str_replace( '_', ' ', $id ) );
+		if ( true === $currency ) {
+			$id_string .= ' (' . get_woocommerce_currency_symbol() . ')';
+		} elseif ( ! empty( $label ) ) {
+			$id_string = $label;
+		}
+		return $id_string;
+	}
+
+	// /**
+	// * Get auction history.
+	// *
+	// * @param int $post_id Product id.
+	// * @return string
+	// */
+	// public static function get_auction_history( $post_id ) {
+	// global $wpdb;
+	// $auction_history = $wpdb->get_results( $wpdb->prepare( 'SELECT * FROM ' . $wpdb->prefix . 'auction_software_logs WHERE auction_id = %d ORDER BY id DESC', array( $post_id ) ), ARRAY_A ); // db call ok; no-cache ok.
+	// if ( ! empty( $auction_history ) ) {
+	// $auction_date_format = get_option( 'date_format' ) . ' ' . get_option( 'time_format' );
+	// $timezone_string     = get_option( 'timezone_string' );
+	// if ( ! $timezone_string ) {
+	// $timezone_string = 'UTC' . wp_timezone_string();
+	// }
+	// $auction_history_string = '
+	// <table id="auction_history_table">
+	// <tr id="auction_history_table_heading">
+	// <td>' . __( 'User', 'auction-software' ) . '</td>
+	// <td>' . __( 'Bid', 'auction-software' ) . '</td>
+	// <td>' . __( 'Date', 'auction-software' ) . '</td>
+	// <td>' . __( 'Auto', 'auction-software' ) . '</td>
+	// </tr>
+	// ';
+	// $previous_value         = null;
+	// foreach ( $auction_history as $auction_history_item ) {
+	// $status = $auction_history_item['status'];
+	// if ( 'ended' === $status ) {
+	// if ( $previous_value && $previous_value === $status ) {
+	// $auction_history_item_string = '';
+	// } else {
+	// $auction_history_item_string = '
+	// <tr>
+	// <td>' . __( 'Auction has ended.', 'auction-software' ) . '</td>
+	// <td></td>
+	// <td>' . gmdate( $auction_date_format, strtotime( $auction_history_item['date'] ) ) . '</td>
+	// <td></td>
+	// </tr>
+	// ';
+	// }
+	// } elseif ( 'relisted' === $status ) {
+	// $auction_history_item_string = '
+	// <tr>
+	// <td>' . __( 'Auction is relisted.', 'auction-software' ) . '</td>
+	// <td></td>
+	// <td>' . gmdate( $auction_date_format, strtotime( $auction_history_item['date'] ) ) . '</td>
+	// <td></td>
+	// </tr>
+	// ';
+	// } elseif ( 'buyitnow' === $status ) {
+	// $user_info                   = get_userdata( $auction_history_item['user_id'] );
+	// $auction_history_item_string = '
+	// <tr>
+	// <td>' . __( 'Buy it now used by ', 'auction-software' ) . $user_info->display_name . '.</td>
+	// <td>' . wc_price( $auction_history_item['bid'] ) . '</td>
+	// <td>' . gmdate( $auction_date_format, strtotime( $auction_history_item['date'] ) ) . '</td>
+	// <td></td>
+	// </tr>
+	// ';
+	// } else {
+	// $user_info                   = get_userdata( $auction_history_item['user_id'] );
+	// $proxy_text                  = ( 1 === (int) $auction_history_item['proxy'] ) ? 'Auto' : '';
+	// $auction_history_item_string = '
+	// <tr>
+	// <td>' . $user_info->display_name . '</td>
+	// <td>' . wc_price( $auction_history_item['bid'] ) . '</td>
+	// <td>' . gmdate( $auction_date_format, strtotime( $auction_history_item['date'] ) ) . '</td>
+	// <td>' . $proxy_text . '</td>
+	// </tr>
+	// ';
+	// }
+	// $previous_value          = $status;
+	// $auction_history_string .= $auction_history_item_string;
+	// }
+	// $auction_history_string .= '</table>';
+	// $get_auction_date_from   = get_post_meta( $post_id, 'auction_date_from' );
+	// $auction_date_from       = '';
+	// if ( isset( $get_auction_date_from[0] ) ) {
+	// $auction_date_from = $get_auction_date_from[0];
+	// }
+	// $auction_history_string .= '<br><p>' . __( 'Started On ', 'auction-software' ) . gmdate( $auction_date_format, strtotime( $auction_date_from ) ) . ' (' . $timezone_string . ')</p>';
+	// return $auction_history_string;
+	// } else {
+	// $auction_history_string = '
+	// <table id="auction_history_table">
+	// <tr id="auction_history_table_heading">
+	// <td>' . __( 'User', 'auction-software' ) . '</td>
+	// <td>' . __( 'Bid', 'auction-software' ) . '</td>
+	// <td>' . __( 'Date', 'auction-software' ) . '</td>
+	// <td>' . __( 'Auto', 'auction-software' ) . '</td>
+	// </tr>
+	// <tr>
+	// <td colspan="4">No bids yet</td>
+	// </tr>
+	// </table>
+	// ';
+	// return $auction_history_string;
+	// }
+	// }
+
+	// /**
+	// * Clear auction logs in case of relisting auction.
+	// *
+	// * @param int  $post_id Product ID.
+	// * @param bool $flag Flag.
+	// */
+	// public static function clear_auction_bid_logs( $post_id, $flag = false ) {
+	// global $wpdb;
+	// if ( $flag ) {
+	// $wpdb->query( $wpdb->prepare( 'DELETE FROM ' . $wpdb->prefix . 'auction_software_logs WHERE auction_id = %d and status IS NOT NULL', array( $post_id ) ) ); // db call ok; no-cache ok.
+	// } else {
+	// $wpdb->query( $wpdb->prepare( 'DELETE FROM ' . $wpdb->prefix . 'auction_software_logs WHERE auction_id = %d', array( $post_id ) ) ); // db call ok; no-cache ok.
+	// }
+	// }
+
+	// /**
+	// * Get auction logs.
+	// *
+	// * @param int    $user_id User id.
+	// * @param int    $post_id Product id.
+	// * @param double $next_bid Next bid.
+	// * @param string $date Date.
+	// * @param null   $status Status.
+	// * @param int    $proxy Is proxy bid.
+	// * @return int
+	// */
+	// public static function set_auction_bid_logs( $user_id, $post_id, $next_bid, $date, $status = null, $proxy = 0 ) {
+	// global $wpdb;
+	// $table_name = $wpdb->prefix . 'auction_software_logs';
+	// $success    = $wpdb->insert(
+	// $table_name,
+	// array(
+	// 'user_id'    => $user_id,
+	// 'auction_id' => $post_id,
+	// 'bid'        => $next_bid,
+	// 'date'       => $date,
+	// 'status'     => $status,
+	// 'proxy'      => $proxy,
+	// )
+	// ); // db call ok; no-cache ok.
+
+	// if ( $success ) {
+	// update_post_meta( $post_id, 'auction_current_bid', sanitize_text_field( $next_bid ) );
+	// return 1;
+	// } else {
+	// return 0;
+	// }
+	// }
+
+	// /**
+	// * Get product meta data by key.
+	// *
+	// * @param int $post_id Product id.
+	// * @param int $key Meta key.
+	// * @return int
+	// */
+	// public static function get_auction_post_meta( $post_id, $key ) {
+	// $get_auction_post_meta = get_post_meta( $post_id, $key );
+	// $auction_post_meta     = 0;
+	// if ( isset( $get_auction_post_meta[0] ) ) {
+	// $auction_post_meta = $get_auction_post_meta[0];
+	// }
+	// return $auction_post_meta;
+	// }
+
+	// /**
+	// * Check if user notified for auction start.
+	// *
+	// * @param int $user_id User id.
+	// * @param int $post_id Product id.
+	// * @return int
+	// */
+	// public static function check_if_user_notified_auction_start( $user_id, $post_id ) {
+	// return self::get_auction_post_meta( $post_id, 'auction_notify_' . $user_id . '_' . $post_id . '_is_started' );
+	// }
+
+	// /**
+	// * Check if user notified for auction ended.
+	// *
+	// * @param int $user_id User id.
+	// * @param int $post_id Product id.
+	// * @return int
+	// */
+	// public static function check_if_user_notified_auction_end( $user_id, $post_id ) {
+	// return self::get_auction_post_meta( $post_id, 'auction_notify_' . $user_id . '_' . $post_id . '_is_ended' );
+	// }
+
+	// /**
+	// * Check if user notified for auction won.
+	// *
+	// * @param int $user_id User id.
+	// * @param int $post_id Product id.
+	// * @return int
+	// */
+	// public static function check_if_user_notified_auction_win( $user_id, $post_id ) {
+	// return self::get_auction_post_meta( $post_id, 'auction_notify_' . $user_id . '_' . $post_id . '_is_won' );
+	// }
+
+	// /**
+	// * Get bid increment by range.
+	// *
+	// * @param double $cur_bid Current bid.
+	// * @return float|string
+	// */
+	// public static function get_auction_bid_increment_by_range( $cur_bid ) {
+	// $bid_incr = '';
+	// $ranges   = get_terms(
+	// 'product_auction_class',
+	// array(
+	// 'hide_empty' => '0',
+	// )
+	// );
+	// foreach ( $ranges as $range ) {
+	// if ( $cur_bid >= $range->name && $cur_bid <= $range->slug ) {
+	// $bid_incr = $range->description;
+	// }
+	// }
+	// $highest_bid_range_increment_value = 0.01;
+	// foreach ( $ranges as $range ) {
+	// if ( $highest_bid_range_increment_value < $range->description ) {
+	// $highest_bid_range_increment_value = $range->description;
+	// }
+	// }
+	// if ( '' === $bid_incr ) {
+	// $bid_incr = $highest_bid_range_increment_value;
+	// }
+	// return $bid_incr;
+	// }
+
+	// /**
+	// * Get time left for auction.
+	// *
+	// * @param int $product_id Product id.
+	// * @return array
+	// */
+	// public static function get_time_left_for_auction( $product_id ) {
+	// $time_to    = self::get_auction_post_meta( $product_id, 'auction_date_to' );
+	// $time_from  = self::get_auction_post_meta( $product_id, 'auction_date_from' );
+	// $return_arr = array();
+	// try {
+	// $date_to = new DateTime( $time_to );
+	// } catch ( Exception $e ) {
+	// return $return_arr;
+	// }
+	// try {
+	// $date_from = new DateTime( $time_from );
+	// } catch ( Exception $e ) {
+	// return $return_arr;
+	// }
+	// try {
+	// $date_cur = new DateTime( current_time( 'mysql' ) );
+	// } catch ( Exception $e ) {
+	// return $return_arr;
+	// }
+	// if ( $date_from > $date_cur ) { // Future auction.
+	// $is_future = true;
+	// array_push( $return_arr, $time_from );
+	// array_push( $return_arr, $is_future );
+	// return $return_arr;
+	// } elseif ( $date_to > $date_cur ) {
+	// array_push( $return_arr, $time_to );
+	// return $return_arr;
+	// } else {
+	// array_push( $return_arr, 0 );
+	// return $return_arr;
+	// }
+	// }
+
+	// /**
+	// * Returns my auctions list.
+	// *
+	// * @param int $user_id User id.
+	// * @return array|int
+	// */
+	// public static function get_auctions_list_products( $user_id ) {
+	// global $woocommerce,$wpdb;
+
+	// if ( ! is_user_logged_in() ) {
+	// return;
+	// }
+	// $post_ids      = array();
+	// $user_auctions = $wpdb->get_results( $wpdb->prepare( 'SELECT  DISTINCT auction_id FROM ' . $wpdb->prefix . 'auction_software_logs WHERE user_id = %d', array( $user_id ) ), ARRAY_N ); // db call ok; no-cache ok.
+	// if ( isset( $user_auctions ) && ! empty( $user_auctions ) ) {
+	// foreach ( $user_auctions as $auction ) {
+	// $post_ids[] = $auction[0];
+	// }
+	// } else {
+	// return;
+	// }
+
+	// $auction_types = apply_filters(
+	// 'auction_software_auction_types',
+	// array(
+	// 'auction_simple',
+	// 'auction_reverse',
+	// )
+	// );
+
+	// $query_args               = array(
+	// 'posts_per_page' => 10,
+	// 'no_found_rows'  => 1,
+	// 'post_status'    => 'publish',
+	// 'post_type'      => 'product',
+	// );
+	// $query_args['post__in']   = $post_ids;
+	// $query_args['meta_query'] = $woocommerce->query->get_meta_query(); // phpcs:ignore slow query
+	// $query_args['tax_query']  = array( // phpcs:ignore slow query
+	// array(
+	// 'taxonomy' => 'product_type',
+	// 'field'    => 'slug',
+	// 'terms'    => $auction_types,
+	// ),
+	// );
+
+	// $results = new WP_Query( $query_args );
+
+	// if ( ! empty( $results ) ) {
+	// return $results;
+	// } else {
+	// return 0;
+	// }
+	// }
+
+	// /**
+	// * Get auction user by Status.
+	// *
+	// * @param int $post_id Product id.
+	// * @return array
+	// */
+	// public static function get_auction_user_by_status( $post_id ) {
+	// global $wpdb;
+	// $user = $wpdb->get_results( $wpdb->prepare( 'SELECT user_id FROM ' . $wpdb->prefix . 'auction_software_logs WHERE auction_id = %d AND status = %s', array( $post_id, 'buyitnow' ) ), ARRAY_A ); // db call ok; no-cache ok.
+
+	// if ( ! empty( $user ) ) {
+	// return $user[0]['user_id'];
+	// } else {
+	// return '';
+	// }
+	// }
+	// /**
+	// * Get Bid Won user by Auction.
+	// *
+	// * @param int $post_id Product id.
+	// * @return array
+	// */
+	// public static function get_won_user_by_auction( $post_id ) {
+	// global $wpdb;
+
+	// $highest_bid = self::get_auction_post_meta( $post_id, 'auction_winning_bid' );
+	// $user        = $wpdb->get_results( $wpdb->prepare( 'SELECT user_id FROM ' . $wpdb->prefix . 'auction_software_logs WHERE auction_id = %d AND bid = %d', array( $post_id, $highest_bid ) ), ARRAY_A ); // db call ok; no-cache ok.
+
+	// if ( ! empty( $user ) ) {
+	// return $user[0]['user_id'];
+	// } else {
+	// return '';
+	// }
+	// }
+}
