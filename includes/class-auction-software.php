e7182a4804d4611f3e170366c2d161dd1477a784
Sonali Londhe <81282336+sonali-londhe@users.noreply.github.com>
2021-08-13 13:05:01 +0530

    Wcfm revert (#60)
    * Added tabs for custom product types
    
    * Added js for relist and extend options for auction.
    
    * Updated code to save products.
    
    * Displayed product in frontend.
    
    * Commented: Cannot redeclare tab error thrown by test case
    
    * Added a condition for redeclaration of auction history tab
    
    * Added a condition for redeclaration of auction history tab
    
    * Added hooks if WCFM class exists.
    
    * Removed auction history tab from WCFM
    
    * Added a condition for declaring auction history
    
    * Added alerts according to user input in the form.
    
    * Added condition in for reverse auction history tab

--- includes/class-auction-software.php
+++ includes/class-auction-software.php
@@ -189,8 +189,6 @@ class Auction_Software {
 
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'woocommerce/classes/products/class-wc-product-auction-reverse.php';
 
-		// wcfm helper file
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'wcfm/class-wcfm-auction-software-helper.php';
 		$this->loader = new Auction_Software_Loader();
 
 	}
@@ -247,14 +245,6 @@ class Auction_Software {
 		$this->loader->add_action( 'woocommerce_account_auctions_list_endpoint', $plugin_admin, 'auction_software_auctions_list_endpoint' );
 		// #My Auctions List Code.
 		$this->loader->add_filter( 'plugin_action_links_' . AUCTION_SOFTWARE_PLUGIN_BASENAME, $plugin_admin, 'auction_software_plugin_action_links' );
-
-		// WCFM plugin hooks and filters.
-		if ( class_exists( 'WCFM' ) ) {
-			$this->loader->add_filter( 'wcfm_product_types', $plugin_admin, 'auction_software_product_auction_types' );
-			$this->loader->add_action( 'after_wcfm_products_manage_general', $plugin_admin, 'wcfm_auction_software_product_auction_tabs', 10, 2 );
-			$this->loader->add_filter( 'wcfm_is_allow_catalog', $plugin_admin, 'wcfm_is_allow_catalog' );
-			$this->loader->add_filter( 'wcfm_product_data_factory', $plugin_admin, 'wcfm_product_data_factory', 13, 4 );
-		}
 	}
 
 	/**
