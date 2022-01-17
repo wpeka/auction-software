12fc3ce0027e9a2308235957d78ff42bc58a2d4d
Rishabh <rishabhtiwari821@gmail.com>
2022-01-17 12:08:06 +0530

    recently viewed is working

--- widgets/class-auction-software-widget-loader.php
+++ widgets/class-auction-software-widget-loader.php
@@ -207,6 +207,7 @@ final class new_Plugin_Extension {
 		require_once( plugin_dir_path(__FILE__) . '/elementor/class-auction-software-widget-my-auctions.php' );
 		require_once( plugin_dir_path(__FILE__) . '/elementor/class-auction-software-widget-random-auctions.php' );
 		require_once( plugin_dir_path(__FILE__) . '/elementor/class-auction-software-widget-recent-auctions.php' );
+		require_once( plugin_dir_path(__FILE__) . '/elementor/class-auction-software-widget-recently-viewed-auctions.php' );
 
 		// Register widget
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Widget_Ending_Soon() );
@@ -215,6 +216,7 @@ final class new_Plugin_Extension {
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Widget_My_Auctions() );
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Widget_Random_Auctions() );
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Widget_Recent_Auctions() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Widget_Recently_Viewed_Auctions() );
 
 	}
 
